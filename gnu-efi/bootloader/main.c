0c567443c452f926834c542b682c60dedc0b8375
Ian-Moffett <hscorp99906@gmail.com>
2022-03-09 14:33:29 -0500

    First Commit.

---
+++ gnu-efi/bootloader/main.c
@@ -0,0 +1,13 @@
+#include <efi.h>
+#include <efilib.h>
+#include <stddef.h>
+
+
+int memcmp(const void* aptr, const void* bptr, size_t n) {
+	const unsigned char* a = aptr, *b = bptr;
+	for (size_t i = 0; i < n; i++) {
+		if (a[i] < b[i]) return -1;
+		else if (a[i] > b[i]) return 1;
+	}
+	return 0;
+}
